5fadc502bcdd9999d4866fcd43aa32c45ed223d7
SOURAV GUPTA <118287650+SURVI1508@users.noreply.github.com>
2023-02-01 22:39:06 +0530

    footerProb

--- pages/Components/Footer.jsx
+++ pages/Components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
                             height={26.23}
                             width={176}
                         />
-                        <p class="mt-5">Unacademy is democratising education, making it accessible to all. Join the revolution, learn on India's largest learning platform.</p>
+                        <p class="mt-5">Unacademy is democratising education, making it accessible to all. Join the revolution, learn on Indias largest learning platform.</p>
                     </div>
                     <div class="d-flex gap-3">
 
